0c5e7159a07f675a7215d79c624ac5e403d6aea8
Gregory Essertel <gregory.essertel@gmail.com>
2014-05-30 17:22:53 -0400

    Added test in test_confusion

--- test/test_confusion.lua
+++ test/test_confusion.lua
@@ -15,12 +15,22 @@ cm:add(prediction, target)
 
 batch_size = 8
 
+-- add batch
 targets = torch.randperm(batch_size)
 predictions = torch.randn(batch_size, #classes)
 
 print'ConfusionMatrix:batchAdd() test'
 cm:batchAdd(predictions, targets)
 assert(cm.mat:sum() == batch_size + 1, 'missing examples')
+
+-- add batch size 1
+targets = torch.FloatTensor({5})
+predictions = torch.randn(1, #classes)
+
+print'ConfusionMatrix:batchAdd() test 2, batch size 1'
+cm.prediction_batch = nil -- because we are using different batchsize
+cm:batchAdd(predictions, targets)
+assert(cm.mat:sum() == batch_size + 2, 'missing examples')
 
 print'ConfusionMatrix:updateValids() test'
 cm:updateValids()
